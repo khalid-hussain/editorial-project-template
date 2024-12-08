7a62330331afd4a5b916c9728e45b7c07501f9e0
Khalid Hussain <mohammad.khalid.hussain@gmail.com>
2024-12-09 01:36:53 +0500

    Initial commit

---
+++ utils/gpp/honorifics.cpp
@@ -0,0 +1,39 @@
+
+\mode{standard}{cpp}
+
+#define hon(phrase, char_style) [phrase]{custom-style="char_style" lang="ar" dir="rtl"}
+
+// Character styles
+#define HEADER honorifics_header
+#define BODY honorifics_body
+#define FOOTER honorifics_foot
+
+// Honorifics
+#define SWT سبحانه وتعالى
+#define AZWJL عزَّ وجلَّ
+#define PBUH صلى الله عليه وسلم
+
+#define S_HIM عليه السلام
+#define S_HER عليها السلام
+#define S_TWO عليهما السلام
+#define S_TWO عليهم السلام
+
+#define SS_HIM عليه الصلاة والسلام
+#define SS_HER عليه الصلاة والسلام
+#define SS_TWO عليهما الصلاة والسلام
+#define SS_THM عليهم الصلاة والسلام
+
+#define P_HIM رضي الله عنه
+#define P_HER رضي الله عنها
+#define P_TWO رضي الله عنهما
+#define P_THM رضي الله عنهم
+
+#define M_HIM رحمه الله تعالى
+#define M_HER رحمها الله تعالى
+#define M_TWO رحمهما الله تعالى
+#define M_THM رحمهم الله تعالى
+
+#define H_HIM حفظه الله تعالى
+#define H_HER حفظها الله تعالى
+#define H_TWO حفظهما الله تعالى
+#define H_THM حفظهم الله تعالى
