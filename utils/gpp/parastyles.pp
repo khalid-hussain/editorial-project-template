a543adebd5a2ac514ee7db9ba40482871fd3d6c3
Khalid Hussain <mohammad.khalid.hussain@gmail.com>
2024-12-11 20:20:41 +0500

    Added para styles macros

--- utils/gpp/parastyles.pp
+++ utils/gpp/parastyles.pp
@@ -7,4 +7,13 @@
 \define{\surahref{surah}{ayahValue}}{(\SURAH_NAME{\surah}, Q\surah:\ayahValue)}
 \endif
 
+\define{quran_ar}{quran-arabic}
+\define{quran_trans}{quran-trans}
+
+\define{hadith_ar}{hadith-arabic}
+\define{hadith_trans}{hadith-trans}
+
+\define{arabic_text}{arabic-text}
+\define{arabic_trans}{arabic-trans}
+
 \mode{standard}{default}
