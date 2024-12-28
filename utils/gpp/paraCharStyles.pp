ff666c5b1b07fa69b347e0d90d3f6db591f7c838
Khalid Hussain <mohammad.khalid.hussain@gmail.com>
2024-12-28 05:07:03 +0500

    Updated from the Sealed Nectar

--- utils/gpp/paraCharStyles.pp
+++ utils/gpp/paraCharStyles.pp
@@ -1,32 +1,67 @@
 
 #mode standard tex
 
+\define{\SC{term}}{[\term]{.smallcaps}}
+
 \ifndef{LONG_SURAH_REF}
 \define{\surahref{surah}{ayahValue}}{(Q\surah:\ayahValue)}
 \else
 \define{\surahref{surah}{ayahValue}}{(\SURAH_NAME{\surah}, Q\surah:\ayahValue)}
 \endif
 
+
+\ifdef{WORD}
+\define{AYAH_AR_START}{::: {custom-style="quran-arabic" lang="ar" dir="rtl"}}
+\define{AYAH_AR_END}{:::}
+\define{AYAH_START}{::: {custom-style="quran-trans"}}
+\define{AYAH_END}{:::}
+
+\define{LIST_AYAH_AR_START}{::: {custom-style="list-quran-arabic" lang="ar" dir="rtl"}}
+\define{LIST_AYAH_AR_END}{:::}
+\define{LIST_AYAH_START}{::: {custom-style="list-quran-trans"}}
+\define{LIST_AYAH_END}{:::}
+
+\define{HADITH_AR_START}{::: {custom-style="hadith-arabic" lang="ar" dir="rtl"}}
+\define{HADITH_AR_END}{:::}
+\define{HADITH_START}{::: {custom-style="hadith-trans"}}
+\define{HADITH_END}{:::}
+
+\define{LIST_HADITH_AR_START}{::: {custom-style="list-hadith-arabic" lang="ar" dir="rtl"}}
+\define{LIST_HADITH_AR_END}{:::}
+\define{LIST_HADITH_START}{::: {custom-style="list-hadith-trans"}}
+\define{LIST_HADITH_END}{:::}
+
+\define{AR_START}{::: {custom-style="hadith-arabic" lang="ar" dir="rtl"}}
+\define{AR_END}{:::}
+\define{EN_START}{::: {custom-style="hadith-trans"}}
+\define{EN_END}{:::}
+\endif
+
 \ifdef{EPUB}
-\define{quran_ar}{quran-arabic}
-\define{quran_trans}{quran-trans}
+\define{AYAH_AR_START}{<div class="quran-arabic" lang="ar" dir="rtl">}
+\define{AYAH_AR_END}{</div>}
+\define{AYAH_START}{<div class="quran-trans">}
+\define{AYAH_END}{</div>}
 
-\define{hadith_ar}{hadith-arabic}
-\define{hadith_trans}{hadith-trans}
+\define{LIST_AYAH_AR_START}{<div class="list-quran-arabic" lang="ar" dir="rtl">}
+\define{LIST_AYAH_AR_END}{</div>}
+\define{LIST_AYAH_START}{<div class="list-quran-trans">}
+\define{LIST_AYAH_END}{</div>}
 
-\define{arabic_text}{arabic-text}
-\define{arabic_trans}{arabic-trans}
+\define{HADITH_AR_START}{<div class="hadith-arabic" lang="ar" dir="rtl">}
+\define{HADITH_AR_END}{</div>}
+\define{HADITH_START}{<div class="hadith-trans">}
+\define{HADITH_END}{</div>}
 
-\else
+\define{LIST_HADITH_AR_START}{<div class="list-hadith-arabic" lang="ar" dir="rtl">}
+\define{LIST_HADITH_AR_END}{</div>}
+\define{LIST_HADITH_START}{<div class="list-hadith-trans">}
+\define{LIST_HADITH_END}{</div>}
 
-\define{quran_ar}{quran-arabic}
-\define{quran_trans}{quran-trans}
-
-\define{hadith_ar}{hadith-arabic}
-\define{hadith_trans}{hadith-trans}
-
-\define{arabic_text}{arabic-text}
-\define{arabic_trans}{arabic-trans}
+\define{AR_START}{<div class="hadith-arabic" lang="ar" dir="rtl">}
+\define{AR_END}{</div>}
+\define{EN_START}{<div class="hadith-trans">}
+\define{EN_END}{</div>}
 \endif
 
 \mode{standard}{default}
